fa61ba7c9da8e41542566009041dedd262fe5a0e
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2020-09-15 19:54:48 -0300

    add segment component

--- src/app/elements/elements.module.ts
+++ src/app/elements/elements.module.ts
@@ -6,11 +6,12 @@ import { ElementComponent } from "./element/element.component";
 import { PlaceHolderComponent } from './place-holder/place-holder.component';
 import { TimesDirective } from './times.directive';
 import { SharedModule } from '../shared/shared.module';
+import { SegmentComponent } from './segment/segment.component';
 
 
 
 @NgModule({
-  declarations: [ElementComponent, PlaceHolderComponent, TimesDirective],
+  declarations: [ElementComponent, PlaceHolderComponent, TimesDirective, SegmentComponent],
   imports: [
     CommonModule,
     ElementsRoutingModule,
