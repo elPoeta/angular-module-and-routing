b17b20977ee3a7f303e3a7ed4cd3cb3240cc8211
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2020-09-15 21:21:40 -0300

    set data table

--- src/app/collections/collection/collection.component.ts
+++ src/app/collections/collection/collection.component.ts
@@ -6,7 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./collection.component.css']
 })
 export class CollectionComponent implements OnInit {
-
+  data = [
+    { name: 'Leonardo', age: 41, job: 'Developer' },
+    { name: 'James', age: 24, job: 'Engineer' },
+    { name: 'Jill', age: 26, job: 'Designer' },
+    { name: 'Elyse', age: 31, job: 'Engineer' }
+  ]
+  headers = [
+    { key: 'name', value: "Nmae" },
+    { key: 'age', valur: 'Age' },
+    { key: 'job', value: 'Job' }
+  ]
   constructor() { }
 
   ngOnInit(): void {
