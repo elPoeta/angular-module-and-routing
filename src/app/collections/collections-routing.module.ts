6714da6c980ede2b8f6e39ba46053c4ee3c03524
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2020-09-15 21:58:18 -0300

    add childre routes for collection

--- src/app/collections/collections-routing.module.ts
+++ src/app/collections/collections-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BiographyComponent } from './biography/biography.component';
 import { CollectionComponent } from './collection/collection.component';
+import { CompaniesComponent } from './companies/companies.component';
+import { PartnersComponent } from './partners/partners.component';
 
 
 const routes: Routes = [
-  { path: '', component: CollectionComponent }
+  {
+    path: '', component: CollectionComponent,
+    children: [
+      { path: '', component: BiographyComponent },
+      { path: 'companies', component: CompaniesComponent },
+      { path: 'partners', component: PartnersComponent }
+    ]
+  }
 ];
 
 @NgModule({
