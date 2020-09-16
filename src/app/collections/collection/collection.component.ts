4d933b2a16577f80a05f376a10d4bd48a407b9c3
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2020-09-15 21:35:11 -0300

    print data on table

--- src/app/collections/collection/collection.component.ts
+++ src/app/collections/collection/collection.component.ts
@@ -8,13 +8,13 @@ import { Component, OnInit } from '@angular/core';
 export class CollectionComponent implements OnInit {
   data = [
     { name: 'Leonardo', age: 41, job: 'Developer' },
-    { name: 'James', age: 24, job: 'Engineer' },
+    { name: 'James', job: 'Engineer', age: 24 },
     { name: 'Jill', age: 26, job: 'Designer' },
     { name: 'Elyse', age: 31, job: 'Engineer' }
   ]
   headers = [
-    { key: 'name', value: "Nmae" },
-    { key: 'age', valur: 'Age' },
+    { key: 'name', value: "Name" },
+    { key: 'age', value: 'Age' },
     { key: 'job', value: 'Job' }
   ]
   constructor() { }
