5394991942adea1b8d36ab1c9057702e17f2e315
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2020-09-15 22:52:06 -0300

    add statistics component

--- src/app/views/views.module.ts
+++ src/app/views/views.module.ts
@@ -3,13 +3,16 @@ import { CommonModule } from '@angular/common';
 
 import { ViewsRoutingModule } from './views-routing.module';
 import { ViewComponent } from './view/view.component';
+import { StatisticsComponent } from './statistics/statistics.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
-  declarations: [ViewComponent],
+  declarations: [ViewComponent, StatisticsComponent],
   imports: [
     CommonModule,
-    ViewsRoutingModule
+    ViewsRoutingModule,
+    SharedModule
   ],
   exports: [
     ViewComponent
