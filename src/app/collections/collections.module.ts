564b4f36af5aaf099843c07e7fac7c605eb3c8e7
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2020-09-15 21:09:44 -0300

    fenerate table component

--- src/app/collections/collections.module.ts
+++ src/app/collections/collections.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { CollectionsRoutingModule } from './collections-routing.module';
 import { CollectionComponent } from './collection/collection.component';
 import { SharedModule } from '../shared/shared.module';
+import { TableComponent } from './table/table.component';
 
 
 @NgModule({
-  declarations: [CollectionComponent],
+  declarations: [CollectionComponent, TableComponent],
   imports: [
     CommonModule,
     CollectionsRoutingModule,
